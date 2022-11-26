e7c946c3152b00012914e74358dac601ac9b5bc0
AdlyRadwika <c30109200033@aeu.edu.my>
2022-11-27 00:47:16 +0700

    Flutter's -version: upgraded to 3.3

--- lib/presentation/pages/watchlist_movies_page.dart
+++ lib/presentation/pages/watchlist_movies_page.dart
@@ -68,7 +68,7 @@ class _WatchlistMoviesPageState extends State<WatchlistMoviesPage>
 
   Consumer<WatchlistMovieNotifier> buildMovieConsumer() {
     return Consumer<WatchlistMovieNotifier>(
-      builder: (context, movieData, child) {
+      builder: (context, movieData,child) {
         if (movieData.watchlistState == RequestState.Loading) {
           return Center(
             child: CircularProgressIndicator(),
@@ -79,10 +79,7 @@ class _WatchlistMoviesPageState extends State<WatchlistMoviesPage>
               itemCount: movieData.watchlistMovies.length,
               itemBuilder: (context, index) {
                 final movie = movieData.watchlistMovies[index];
-                return EntertaimentCard(
-                  movie: movie,
-                  isTV: false,
-                );
+                return EntertaimentCard(movie: movie, isTV: false,);
               },
             ),
           );
@@ -100,7 +97,7 @@ class _WatchlistMoviesPageState extends State<WatchlistMoviesPage>
 
   Consumer<WatchlistTvNotifier> buildTvConsumer() {
     return Consumer<WatchlistTvNotifier>(
-      builder: (context, tvData, child) {
+      builder: (context, tvData,child) {
         if (tvData.watchlistState == RequestState.Loading) {
           return Center(
             child: CircularProgressIndicator(),
@@ -111,10 +108,7 @@ class _WatchlistMoviesPageState extends State<WatchlistMoviesPage>
               itemCount: tvData.watchlistTvs.length,
               itemBuilder: (context, index) {
                 final tv = tvData.watchlistTvs[index];
-                return EntertaimentCard(
-                  tv: tv,
-                  isTV: true,
-                );
+                return EntertaimentCard(tv: tv, isTV: true,);
               },
             ),
           );
